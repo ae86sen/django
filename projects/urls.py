e4a61dbd9257fc305f683ec31b5914e837db4db2
ae86sen <listeningsss@163.com>
2020-12-21 23:11:13 +0800

    day02: 1、函数视图和类视图 2、请求参数的获取（路径参数、查询参数、请求体参数） 3、请求头内容的获取

--- projects/urls.py
+++ projects/urls.py
@@ -5,10 +5,15 @@ Time:2020/12/20
 E-mail:369799130@qq.com
 ============================
 """
-from projects.views import index_page, index_page2
+from projects.views import index_page, index_page2, IndexPage
 from django.urls import path
 
 urlpatterns = [
     path('index/', index_page),
-    path('index2/', index_page2)
+    path('index2/', index_page2),
+    # 类视图定义路由
+    # 1、path的第二个参数为类名.as_view()
+    # 2、可以使用<url类型转化器:路径参数名>
+    # 3、类型包括int、path、uuid、slug等
+    path('index3/<int:page_id>', IndexPage.as_view())
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,53 @@
+import json
+
 from django.http import HttpResponse
+from django.views import View
 
 
 def index_page(request):
-    return HttpResponse("<h2>hello,jack!</h2>")
+    if request.method == 'GET':
+        return HttpResponse("<h2>GET请求：hello,jack!</h2>")
+    elif request.method == 'POST':
+        return HttpResponse("<h2>POST请求：hello,jack!</h2>")
+    elif request.method == 'PUT':
+        return HttpResponse("<h2>PUT请求：hello,jack!</h2>")
+    elif request.method == 'DELETE':
+        return HttpResponse("<h2>DELETE请求：hello,jack!</h2>")
 
 
 def index_page2(request):
     return HttpResponse("<h2>hello,jack222!</h2>")
+
+
+class IndexPage(View):
+    """
+    类视图
+    1、必须要继承View
+    2、可以定义get,post,put,delete方法，来实现GET，POST,PUT,DELETE请求
+    3、get,post,put,delete方法名称固定且均为小写
+    4、实例方法的第二个参数为HttpRequest对象或其子类对象
+    5、一定要返回一个HttpResponse对象
+    """
+
+    def get(self,request,page_id):
+        # 5、url后面的？号参数，成为query string查询字符串参数
+        # 6、request.GET去获取参数字符串参数
+        # 7、request.GET返回QueryDict对象，类似于一个字典，支持字典中的所有操作
+        # 8、request.GET[key]、request.GET.get[key]、request.GET.getlist()去获取参数值
+        return HttpResponse("<h2>GET请求：hello,jack!</h2>")
+
+    def post(self,request,page_id):
+        # a、可以使用request.POST方法，去获取application/x-www-urldecoded
+        # b、可以使用request.body方法，去获取application/json类型的参数
+        # c、可以使用request.META方法（或request.headers方法），去获取请求头参数，key为HTTP_请求头key的大写
+        data = json.loads(request.body,encoding='utf-8')
+        return HttpResponse(f"<h2>POST请求：hello,{data['name']}!</h2>")
+
+    def put(self,request,page_id):
+        # a、HttpResponse对象，第一个参数为字符串类型或字节类型，会将字符串内容返回给前端
+        # b、可以使用content_type来指定内容类型
+        # c、可以使用status参数来指定响应状态码
+        return HttpResponse("<h2>PUT请求：hello,jack!</h2>")
+
+    def delete(self,request,page_id):
+        return HttpResponse("<h2>DELETE请求：hello,jack!</h2>")
