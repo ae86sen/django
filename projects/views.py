33b0c4881cc418b143c61dd2f2d1006b30de6c4b
ancientone <listeningsss@163.com>
2020-12-25 09:13:29 +0800

    1

--- projects/views.py
+++ projects/views.py
@@ -6,6 +6,7 @@ from django.views import View
 from projects.models import Projects
 from django.db import connection
 
+
 def index_page(request):
     if request.method == 'GET':
         return HttpResponse("<h2>GET请求：hello,jack!</h2>")
@@ -31,14 +32,14 @@ class IndexPage(View):
     5、一定要返回一个HttpResponse对象
     """
 
-    def get(self,request,page_id):
+    def get(self, request, page_id):
         # 5、url后面的？号参数，成为query string查询字符串参数
         # 6、request.GET去获取参数字符串参数
         # 7、request.GET返回QueryDict对象，类似于一个字典，支持字典中的所有操作
         # 8、request.GET[key]、request.GET.get[key]、request.GET.getlist()去获取参数值
         return HttpResponse("<h2>GET请求：hello,jack!</h2>")
 
-    def post(self,request,page_id):
+    def post(self, request, page_id):
         # 一、创建（C）
         # 1、使用模型类对象来创建
         # 会创建一个Projects模型类对象，但是还未提交
@@ -73,14 +74,14 @@ class IndexPage(View):
         # b.get方法如果查询的记录为空和多条记录，那么会抛出异常
         # c.返回的模型类对象，会自动提交
         one = Projects.objects.get(id=1)
-        data = json.loads(request.body,encoding='utf-8')
+        data = json.loads(request.body, encoding='utf-8')
         return HttpResponse(f"<h2>POST请求：hello,{data['name']}!</h2>")
 
-    def put(self,request,page_id):
+    def put(self, request, page_id):
         # a、HttpResponse对象，第一个参数为字符串类型或字节类型，会将字符串内容返回给前端
         # b、可以使用content_type来指定内容类型
         # c、可以使用status参数来指定响应状态码
         return HttpResponse("<h2>PUT请求：hello,jack!</h2>")
 
-    def delete(self,request,page_id):
+    def delete(self, request, page_id):
         return HttpResponse("<h2>DELETE请求：hello,jack!</h2>")
